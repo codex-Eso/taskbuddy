45877b8e5bc49ae4d088be3cf2beee8240c0441e
codex-Eso <esmondlowfh@gmail.com>
2026-08-05 11:39:18 +0800

    Build Bug Fixes

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
-import taskRoutes from "./routes/tasks.ts";
+import taskRoutes from "./routes/tasks.js";
 import { fileURLToPath } from "url";
 
 dotenv.config();
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, ".")));
 const port = process.env.PORT || 4000;
 
 const client = new MongoClient(process.env.MONGO_URI!);
