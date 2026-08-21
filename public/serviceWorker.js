2dfadf91cf92e27c335638524e93dc2b2297eedd
codex-Eso <esmondlowfh@gmail.com>
2026-08-21 16:16:03 +0800

    Fixed serviceWorker once again to prevent bottleneck for features

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -5,7 +5,7 @@ const URLS_TO_CACHE = [
 ];
 
 self.addEventListener("activate", (event) => {
-    const cacheWhitelist = ["taskbuddy-static", "taskbuddy-dynamic"];
+    const cacheWhitelist = ["taskbuddy-static-v1"];
     event.waitUntil(
         caches.keys().then((keys) =>
             Promise.all(
@@ -21,33 +21,18 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("taskbuddy-static").then((cache) => cache.addAll(URLS_TO_CACHE))
+        caches.open("taskbuddy-static-v1").then((cache) => cache.addAll(URLS_TO_CACHE))
     );
 });
 
 self.addEventListener("fetch", (event) => {
-    if (event.request.url.includes("/tasks")) {
-        event.respondWith(
-            fetch(event.request)
-                .then((response) => {
-                    const clone = response.clone();
-                    caches.open("taskbuddy-dynamic").then((cache) => {
-                        cache.put(event.request, clone);
-                    });
-                    return response;
-                })
-                .catch(() => {
-                    return caches.match(event.request);
-                })
-        );
-    }
     if (event.request.url.includes("/assets/")) {
         event.respondWith(
             caches.match(event.request).then((resp) => {
                 return (
                     resp ||
                     fetch(event.request).then((networkResp) => {
-                        caches.open("taskbuddy-static").then((cache) => {
+                        caches.open("taskbuddy-static-v1").then((cache) => {
                             cache.put(event.request, networkResp.clone());
                         });
                         return networkResp;
